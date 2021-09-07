ad68625ad62ff160edc23ae502fd9855848a1a40
maguero73 <maguerotecnico@gmail.com>
2021-09-06 22:04:42 -0300

    Primera version 2021 Full - Curso Java. Ejercicio Práctico I. Vídeo 123

---
+++ src/practicas/Marco_Dialogos.java
@@ -0,0 +1,18 @@
+package practicas;
+
+import javax.swing.*;
+
+public class Marco_Dialogos extends JFrame{
+	
+	public Marco_Dialogos() {
+		
+		setTitle("Prueba de Dialogos");
+		
+		setBounds(500,300,600,450);
+		
+		
+		
+	}
+	
+	
+}
